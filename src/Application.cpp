72ccb0535301193f350dc8634f03fe15cc24172e
Artem Danylov <145837590+adnlv@users.noreply.github.com>
2026-06-14 15:17:53 +0300

    Add Application class

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,6 +1,108 @@
 #include <iostream>
+#include <exception>
+#include <string_view>
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <spdlog/spdlog.h>
+
+class Application
+{
+public:
+	Application();
+	~Application() noexcept;
+
+private:
+	GLFWwindow* m_Window;
+
+	void GLFWInit();
+	void GLFWTerminate() noexcept;
+	void GLFWCreateWindow();
+	void GLFWDestroyWindow() noexcept;
+
+	void GLADLoadGL();
+
+	void Update();
+};
+
+Application::Application()
+{
+	GLFWInit();
+	GLFWCreateWindow();
+	GLADLoadGL();
+	
+	Update();
+}
+
+Application::~Application()
+{
+	GLFWDestroyWindow();
+	GLFWTerminate();
+}
+
+void Application::GLFWInit()
+{
+	if (glfwInit() != GLFW_TRUE)
+	{
+		throw std::runtime_error("GLFW: Initialization failed");
+	}
+	spdlog::info("GLFW: Initialized");
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+}
+
+void Application::GLFWTerminate() noexcept
+{
+	glfwTerminate();
+	spdlog::info("GLFW: Terminated");
+}
+
+void Application::GLFWCreateWindow()
+{
+	m_Window = glfwCreateWindow(960, 540, "Termperature Simulation", nullptr, nullptr);
+	if (m_Window == nullptr)
+	{
+		throw std::runtime_error("GLFW: Window creation failed");
+	}
+	spdlog::info("GLFW: Window created");
+
+	glfwMakeContextCurrent(m_Window);
+}
+
+void Application::GLFWDestroyWindow() noexcept
+{
+	glfwDestroyWindow(m_Window);
+	spdlog::info("GLFW: Window destroyed");
+}
+
+void Application::GLADLoadGL()
+{
+	if (!gladLoadGL(glfwGetProcAddress))
+	{
+		throw std::runtime_error("GLAD: GL loading failed");
+	}
+	spdlog::info("GLAD: GL loaded");
+
+	int frameBufferWidth;
+	int frameBufferHeight;
+	glfwGetFramebufferSize(m_Window, &frameBufferWidth, &frameBufferHeight);
+	glViewport(0, 0, frameBufferWidth, frameBufferHeight);
+}
+
+void Application::Update()
+{
+	while (!glfwWindowShouldClose(m_Window))
+	{
+		glfwSwapBuffers(m_Window);
+		glfwPollEvents();
+	}
+}
 
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	Application app;
 }
