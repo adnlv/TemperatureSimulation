5106db3c2809778e114c60003357ad9c28f22b12
Artem Danylov <145837590+adnlv@users.noreply.github.com>
2026-06-14 15:43:31 +0300

    Clear color buffer with hardcoded color

--- src/Application.cpp
+++ src/Application.cpp
@@ -136,6 +136,9 @@ void Application::Update()
 {
 	while (!glfwWindowShouldClose(m_Window))
 	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
 		glfwSwapBuffers(m_Window);
 		glfwPollEvents();
 	}
